5b6e7d10af80dbacf07e4cb50170849e20ce942c
Vaibhavi Bhaud <your-email@gmail.com>
2026-01-31 17:21:12 +0530

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,17 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+    base: '/My_Portfolio1/',
   resolve: {
     dedupe: ['react', 'react-dom'], // This fixes the useContext error from earlier
   },
 })
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/My_Portfolio1/',
+// })
